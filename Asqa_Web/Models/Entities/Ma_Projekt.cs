fa17505ec170ab9f475264fa41bd6795823d7fc2
GershTheron <gershwin.theron@gmail.com>
2024-07-03 09:32:30 +0100

    index_view_nameadd

--- Asqa_Web/Models/Entities/Ma_Projekt.cs
+++ Asqa_Web/Models/Entities/Ma_Projekt.cs
@@ -9,21 +9,20 @@ namespace Asqa_Web.Models.Entities
         [Key]
         public int Id { get; set; }
 
-        [Required]
+      
         public Guid MitarbeiterId { get; set; }
 
-        [Required]
+       
         public int ProjektId { get; set; }
 
-        [Required]
+        
         public string? MaNachname { get; set; }
 
-        [Required]
         public string? Proj_Name { get; set; }
 
         public string? Rolle { get; set; }
 
-        [Required]
+       
         public DateTime StartDate { get; set; }
 
         public DateTime? EndDate { get; set; }
