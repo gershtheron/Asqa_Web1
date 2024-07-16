de7c18a99bc792dee89462594eb79dd64dba9076
GershTheron <gershwin.theron@gmail.com>
2024-07-16 09:11:54 +0100

    pdfController

---
+++ Asqa_Web/Controllers/DocumentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asqa_Web.Controllers
+{
+    public class DocumentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
