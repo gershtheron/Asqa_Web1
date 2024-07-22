0f8dd260fecdbfb15027a0a259d95a997e0e0e0b
GershTheron <gershwin.theron@gmail.com>
2024-07-22 10:34:36 +0100

    addedRolleToBeraterProjektIndex

---
+++ Asqa_Web/Models/Berater_ProjektViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Asqa_Web.Models
+{
+    public class Berater_ProjektViewModel
+    {
+        public string? Proj_Name { get; set; }
+        public string? Rolle { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public List<string>? TaetigkeitenDescriptions { get; set; }
+    }
+}
